1693b4e0b0ac083f51ee49b3ef3727eef5f0b9af
sengineer-tribot <xwmrccqncpdlpcfxuf@wqcefp.online>
2020-12-04 00:38:27 +0200

    Adding source

---
+++ src/scripts/fm/main/SEngineerFiremaker.java
@@ -0,0 +1,131 @@
+package scripts.fm.main;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.util.Random;
+
+import org.tribot.api.General;
+import org.tribot.api.input.Mouse;
+import org.tribot.api2007.GroundItems;
+import org.tribot.api2007.Skills;
+import org.tribot.api2007.Skills.SKILLS;
+import org.tribot.api2007.WorldHopper;
+import org.tribot.api2007.types.RSGroundItem;
+import org.tribot.api2007.types.RSTile;
+import org.tribot.script.Script;
+import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.Painting;
+
+import scripts.fm.paint.FluffeesPaint;
+import scripts.fm.paint.PaintInfo;
+import scripts.fm.support.ABC2Support;
+import scripts.fm.utils.Utils;
+
+@ScriptManifest(authors = {
+		"SEngineer" }, category = "Firemaking", name = "SEngineer's F2P Firemaker", version = 1.00, description = "F2P Firemaking", gameMode = 1)
+
+/**
+ * 
+ * F2P Firemaking Script
+ * 
+ * This script will firemake logs in Lumbridge Castle
+ * 
+ * @author SEngineer
+ *
+ */
+public class SEngineerFiremaker extends Script implements PaintInfo, Painting {
+	
+	private final Double scriptVersion = 1.00;
+	
+	private final FluffeesPaint display = new FluffeesPaint(
+			this, FluffeesPaint.PaintLocations.TOP_RIGHT_CHATBOX,
+			new Color[] { new Color(255, 251, 255) }, "Trebuchet MS", new Color[] { new Color(93, 156, 236, 127) },
+			new Color[] { new Color(39, 95, 175) }, 1, false, 5, 3, 0);
+
+	// ~~~ Support Classes
+
+	private ABC2Support abc2Support;
+	
+	private Utils utils = new Utils();
+	
+	// ~~~ Script Variables
+
+	private String status = "";
+	private int firemakingXP = 0;
+	
+	private Random rand = new Random();
+	
+	@Override
+	public String[] getPaintInfo() {
+        return new String[] {
+        		"SEngineer's F2P Firemaker v" + String.format("%.2f", scriptVersion),
+        		"Status: " + status,
+        		"Firemaking XP: " + (Skills.getXP(SKILLS.FIREMAKING) - firemakingXP),
+        		"Script Runtime: " + display.getRuntimeString(),
+        };
+	}
+	
+	@Override
+	public void onPaint(final Graphics gfx) {
+		display.paint(gfx);
+	}
+
+	/**
+	 * Configures any script pre-requisites
+	 */
+	private void configure() {
+		General.println("[FireMaker] Configuring SEngineer's F2P FireMaker");
+
+		utils.configureDaxWalker();
+		Mouse.setSpeed(100 + rand.nextInt(30));
+		Mouse.scroll(false, rand.nextInt(20));
+
+		General.useAntiBanCompliance(true);
+		abc2Support = ABC2Support.getInstance();
+		abc2Support.generateTrackers();
+
+		firemakingXP = Skills.getXP(SKILLS.FIREMAKING);
+	}
+
+	/**
+	 * 1. Configure the DaxWalker 
+	 * 2. Firemake logs
+	 */
+	@Override
+	public void run() {
+		configure();
+		
+		if(!utils.inventoryContains(590)) {
+			updateScriptStatus("ERROR: No Tinderbox!");
+			return;
+		}
+		
+		updateScriptStatus("Walking to Lumbridge Castle");
+		utils.walkTo(new RSTile(3209, 3219, 2));
+
+		while (true) {
+			updateScriptStatus("Burning logs");
+			final RSGroundItem[] logs = GroundItems.findNearest(10, 1511);
+			if(logs.length > 0) {
+				for(int i = 0; i < logs.length; i++) {
+					utils.burnLogOnGround(1511);
+				}
+			}
+			else {
+				updateScriptStatus("Hopping worlds");
+				WorldHopper.changeWorld(WorldHopper.getRandomWorld(false));
+			}
+			abc2Support.runAntiBan();
+		}
+	}
+	
+	/**
+	 * Updates the script status
+	 * @param status - Current status of the script
+	 */
+	public void updateScriptStatus(final String status) {
+		General.println(String.format("[FireMaker] %s", status));
+		this.status = status;
+	}
+
+}
---
+++ src/scripts/fm/utils/Utils.java
@@ -0,0 +1,167 @@
+package scripts.fm.utils;
+
+import org.tribot.api.DynamicClicking;
+import org.tribot.api.General;
+import org.tribot.api.Timing;
+import org.tribot.api.interfaces.Positionable;
+import org.tribot.api2007.GroundItems;
+import org.tribot.api2007.Inventory;
+import org.tribot.api2007.Player;
+import org.tribot.api2007.types.RSGroundItem;
+import org.tribot.api2007.types.RSItem;
+
+import scripts.dax_api.api_lib.DaxWalker;
+import scripts.dax_api.api_lib.models.DaxCredentials;
+import scripts.dax_api.api_lib.models.DaxCredentialsProvider;
+
+/**
+ *
+ * Utilities
+ *
+ * This class contains all random Utilities that have not yet been categorized
+ *
+ * @author SEngineer
+ *
+ */
+public class Utils {
+	
+	/**
+	 * Checks if the inventory contains a specific item
+	 * 
+	 * @param item - id of the item to search for
+	 * @return true/false
+	 */
+	public boolean inventoryContains(final int item) {		
+		return Inventory.find(item).length > 0;
+	}
+	
+	
+	/**
+	 * Checks if an item is close to the player
+	 * @param item - item to check for
+	 * @return true/false
+	 */
+	public boolean isItemClose(final int id, final int distance) {
+		final RSGroundItem[] items = GroundItems.findNearest(5, id);
+
+		return items.length > 0 && Player.getPosition().distanceTo(items[0].getPosition()) < distance;
+	}
+	
+	/**
+	 * Burns a log on the ground
+	 * 
+	 * @param logs - log to burn
+	 * @return true/false
+	 */
+	public boolean burnLogOnGround(final int log) {
+		General.println(String.format("[FireMaker] Burning (log %d)", log));
+
+		if (!inventoryContains(590) || !isItemClose(log, 5) || isPlayerPerformingAnimation(733)) {
+			return false;
+		}
+
+		useItemOnGroundItem(590, log);
+
+		General.sleep(1800, 3000);
+
+		Timing.waitCondition(() -> !isPlayerPerformingAnimation(733), 30000);
+
+		return true;
+	}
+	
+	/**
+     * Uses an item in the inventory on a ground item
+     * @param srcItemId - item to use
+     * @param destItemId - item to click
+     * @return true/false
+     */
+    public boolean useItemOnGroundItem(final int srcItemId, final int destItemId) {
+    	final RSGroundItem[] destItems = GroundItems.find(destItemId);
+    	
+    	if(inventoryContains(srcItemId) || destItems.length == 0) { 
+    		return false; 
+    	}
+   
+    	final RSItem srcItem = Inventory.find(srcItemId)[0];
+    	srcItem.click("Use");
+
+		final boolean isUsed = DynamicClicking.clickRSGroundItem(destItems[0], 
+				String.format("%s %s -> %s", "Use", srcItem.getDefinition().getName(), destItems[0].getDefinition().getName()));
+
+		General.sleep(600, 1200);
+
+		return isUsed;
+    }
+
+	/**
+	 * Configures the DaxWalker
+	 */
+	public void configureDaxWalker() {
+		General.println("[FireMaker] Configuring Dax Walker Credentials");
+
+		DaxWalker.setCredentials(new DaxCredentialsProvider() {
+			@Override
+			public DaxCredentials getDaxCredentials() {
+				return new DaxCredentials("sub_DPjXXzL5DeSiPf", "PUBLIC-KEY");
+			}
+		});
+	}
+
+	/**
+	 * Walks to a location if the Player is not already there
+	 * 
+	 * @param destination - place to walk to
+	 */
+	public void walkTo(final Positionable destination) {
+		while (!isPlayerCloseTo(destination)) {
+			General.println(String.format("[FireMaker] Attempting to walk to (%s)", destination.toString()));
+			DaxWalker.walkTo(destination);
+			General.sleep(Numbers.FIVE_SECONDS, Numbers.EIGHT_SECONDS);
+		}
+		General.println(String.format("[FireMaker] Cancelling walk - We are already close to (%s)", destination.toString()));
+	}
+
+	/**
+	 * Checks if the Players current position is within 10 tiles of the destination
+	 * 
+	 * @param destination - place to walk to
+	 * @return true/false
+	 */
+	public boolean isPlayerCloseTo(final Positionable destination) {
+		return Player.getPosition().getPlane() == destination.getPosition().getPlane()
+				? Player.getPosition().distanceTo(destination) < 5 : false;
+	}
+
+	/**
+	 * Checks if an amount of time has elapsed
+	 * 
+	 * @param startTime    - the start time
+	 * @param timeToElapse - the amount of time to elapse
+	 * @return true/false
+	 */
+	public boolean isTimeElapsed(final long startTime, final long time) {
+		return (System.currentTimeMillis() - startTime) > time ? true : false;
+	}
+	
+	/**
+	 * Check if a player is performing a specific animation
+	 * 
+	 * @param animation - animation to check
+	 * @return true/false
+	 */
+	public boolean isPlayerPerformingAnimation(final int animation) {
+		boolean isAnimating = false;
+
+		long start = System.currentTimeMillis();
+
+		while (!isTimeElapsed(start, Numbers.ONE_SECOND)) {
+			if (Player.getAnimation() == animation) {
+				isAnimating = true;
+				break;
+			}
+			General.sleep(200);
+		}
+		return isAnimating;
+	}
+	
+}
